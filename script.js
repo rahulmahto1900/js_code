573d33f52c806eae4ba5c71af24256c69215b101
rahulmahto1900 <rahulmahto1900@gmail.com>
2022-05-16 12:52:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<script> 
+
+let parentElement = document.querySelectorAll('.parent');
+		parentElement.forEach(element => {
+			element.addEventListener("click", function () {
+				[...element.parentElement.children].forEach(el => el.classList.remove('active')); 
+					element.classList.add('active');
+			});
+		})
+
+</script>
